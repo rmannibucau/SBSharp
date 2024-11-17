c40b3a7990808e9e5e6f0546a8f1f7e05bd0cb19
Romain Manni-Bucau <romain.manni@orangelogic.com>
2024-11-17 20:13:38 +0100

    [cache] enhance razorlight template factory caching since default caching misses cache for recursive calls

--- src/SBSharp.Core/SBSharp/Core/View/ViewRenderer.cs
+++ src/SBSharp.Core/SBSharp/Core/View/ViewRenderer.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using Microsoft.Extensions.Logging;
 using RazorLight;
 using RazorLight.Compilation;
@@ -9,29 +10,40 @@ public class ViewRenderer
 {
     private readonly SBSharpConfiguration configuration;
     private readonly RazorLightEngine engine;
+    private readonly ConcurrentDictionary<string, Task<Func<ITemplatePage>>> cache = new();
 
     public ViewRenderer(SBSharpConfiguration configuration, ILogger<ViewRenderer> logger)
     {
         this.configuration = configuration;
 
-        var root = Path.Combine(Directory.GetCurrentDirectory(), configuration.Input.Location, configuration.Input.View);
+        var root = Path.Combine(Directory.GetCurrentDirectory(), configuration.Input.Location,
+            configuration.Input.View);
         logger.LogInformation("Using view directory '{Directory}'", root);
 
         engine = new RazorLightEngineBuilder()
             .SetOperatingAssembly(typeof(ViewRenderer).Assembly)
             .UseFileSystemProject(root)
             .UseMemoryCachingProvider()
-            .UseOptions(new RazorLightOptions {
-                
+            .UseOptions(new RazorLightOptions
+            {
+                EnableDebugMode = false
             })
             .Build();
     }
 
     public async Task<string> RenderAsync(string view, Page page)
     {
+        // razorlight has a caching bug (leading to computing N times > 1 the same view and compilation with roselyn is slow)
+        // so we add our caching layer on top of it
         try
         {
-            return await engine.CompileRenderAsync(view, page).ConfigureAwait(false);
+            var promise = cache.GetOrAdd(view, async k =>
+            {
+                var templateDescriptor = await engine.Handler.Compiler.CompileAsync(k);
+                return engine.Handler.FactoryProvider.CreateFactory(templateDescriptor);
+            });
+            var template = await promise.ConfigureAwait(false);
+            return await engine.RenderTemplateAsync(template(), page).ConfigureAwait(false);
         }
         catch (TemplateCompilationException e)
         {
